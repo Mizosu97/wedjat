bb951e06c1d4b66679ff7bf34348f2a5c6cd1783
Mizosu97 <earthpig97@gmail.com>
2023-02-07 21:35:34 -0500

    moon

---
+++ src/include/token.h
@@ -0,0 +1,17 @@
+#ifndef TOEKN_H
+#define TOKEN_H
+typedef struct TOKEN_STRUCT {
+	enum {
+		TOKEN_ID,
+		TOKEN_EQUALS,
+		TOKEN_STRING,
+		TOKEN_SEMI,
+		TOKEN_LPAREN,
+		TOKEN_RPAREN
+	} type;
+
+	char* value;
+} token_T;
+
+token_T* init_token(int type, char* value);
+#endif
