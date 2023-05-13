8f4976cb89cebdfc7d21e820919abc312df6a7fd
Mizosu97 <earthpig97@gmail.com>
2023-05-13 00:17:39 -0400

    aaa

--- lexer.lua
+++ lexer.lua
@@ -5,16 +5,56 @@ pos = 1
 peekpos = pos + 1
 
 local function getc(str)
-	return string.sub(str, pos, pos)
 	pos = pos + 1
+	return string.sub(str, pos, pos)
 end
 
 local function peek(str)
-	return string.sub(str, peekpos, peekpos)
 	peekpos = peekpos + 1
+	return string.sub(str, peekpos, peekpos)
+end
+
+local function maketoken(tokentype, datatype, value)
+	table.insert(tokens, {tokentype, datatype, value})
+end
+
+local lexswitch = {
+	["\""] = function(str)
+		local tokenv = ""
+		while true do
+			local c = peek(str)
+			if c ~= "\"" then
+				tokenv = tokenv .. c
+				peekpos = peekpos + 1
+			else
+				break
+			end
+		end
+		maketoken("data", "string", tokenv)
+		peekpos = peekpos + 1
+		pos = peekpos - 1
+	end
+}
+
+local function checker(c)
+	if lexswitch[c] then 
+		return true
+	else
+		return false
+	end
 end
 
 lexer.lex = function(str)
+	local strlen = string.len(str)
+	while pos <= strlen do
+		local c = getc(str)
+		if checker(c) == true then
+			lexswitch[c](str)
+		end
+		pos = pos + 1
+		peekpos = pos + 1
+	end
+	return tokens
 end
 
 return lexer
--- main.lua
+++ main.lua
@@ -1,6 +1,15 @@
 -- Created by https://entertheduat.org
 
+local lexer = dofile("/home/mizosu/vscodium/wedjat/lexer.lua")
+
 local file = io.open(arg[1], "r")
 local contents = file:read("*all")
 
-local tokens = lex(contents)
+local tokens = lexer.lex(contents)
+
+
+local tokenindex = 1
+while tokenindex <= #tokens do
+	local token = tokens[tokenindex]
+	print(string.format("\nToken %d:\nToken type: %s\nData type: %s\nValue: %s", tokenindex, token[1], token[2], token[3]))
+end
